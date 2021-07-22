006977149ea880a52a4bf81bfc89b0f6f25048c2
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-07-22 22:04:04 +0200

    build: ajoute le projet spring

---
+++ implicaction-back/src/main/java/com/dynonuggets/sdv/RefonteImplicactionApplication.java
@@ -0,0 +1,13 @@
+package com.dynonuggets.sdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefonteImplicactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefonteImplicactionApplication.class, args);
+	}
+
+}
---
+++ implicaction-back/src/test/java/com/dynonuggets/sdv/RefonteImplicactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynonuggets.sdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefonteImplicactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
