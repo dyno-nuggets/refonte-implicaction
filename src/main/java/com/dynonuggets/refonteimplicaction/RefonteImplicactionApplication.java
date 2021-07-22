a66e15b9c8940bfd59e6f8b631685d54362d9544
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-07-22 22:25:37 +0200

    fix: déplace le projet spring à la racine du projet
     * configuration des properties de la bdd

--- src/main/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplication.java
+++ src/main/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplication.java
@@ -1,4 +1,4 @@
-package com.dynonuggets.sdv;
+package com.dynonuggets.refonteimplicaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplicationTests.java
+++ src/test/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dynonuggets.sdv;
+package com.dynonuggets.refonteimplicaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
