1e9b20ae1d30ef386c25bc48f80b3db8aceccdca
matthieuaudemard <matthieu.audemard@gmail.com>
2021-08-01 06:02:59 +0200

    refact: ajoute différentes amélioration et qques fix (#8)
    * renommage de la classe ImplicitactionException en ImplicactionException
     * factorisation de différentes méthodes dans le service auth

---
+++ backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/exception/ImplicactionException.java
@@ -0,0 +1,7 @@
+package com.dynonuggets.refonteimplicaction.exception;
+
+public class ImplicactionException extends Throwable {
+    public ImplicactionException(String message) {
+        super(message);
+    }
+}
--- backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/service/MailService.java
+++ backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/service/MailService.java
@@ -1,7 +1,8 @@
 package com.dynonuggets.refonteimplicaction.service;
 
 import com.dynonuggets.refonteimplicaction.dto.NotificationEmailDto;
-import com.dynonuggets.refonteimplicaction.exception.ImplicitActionException;
+import com.dynonuggets.refonteimplicaction.exception.ImplicactionException;
+import com.dynonuggets.refonteimplicaction.model.User;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
@@ -10,6 +11,9 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import static com.dynonuggets.refonteimplicaction.util.Constants.ACTIVATION_ENDPOINT;
+import static com.dynonuggets.refonteimplicaction.util.Constants.IMPLICACTION_MAIL_ADDRESS;
 
 @Service
 @AllArgsConstructor
@@ -20,10 +24,10 @@ public class MailService {
     private final JavaMailSender mailSender;
 
     @Async
-    void sendMail(NotificationEmailDto notificationEmail) throws ImplicitActionException {
+    public void sendMail(NotificationEmailDto notificationEmail) throws ImplicactionException {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-            messageHelper.setFrom("implicaction@email.com");
+            messageHelper.setFrom(IMPLICACTION_MAIL_ADDRESS);
             messageHelper.setTo(notificationEmail.getRecipient());
             messageHelper.setSubject(notificationEmail.getSubject());
             messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
@@ -32,7 +36,17 @@ public class MailService {
             mailSender.send(messagePreparator);
             log.info("Activation email sent!!");
         } catch (MailException e) {
-            throw new ImplicitActionException("Exception occurred when sending mail to " + notificationEmail.getRecipient());
+            throw new ImplicactionException("Exception occurred when sending mail to " + notificationEmail.getRecipient());
         }
+    }
+
+    public void sendUserActivationMail(final String activationKey, final User user) throws ImplicactionException {
+        NotificationEmailDto notificationEmail = NotificationEmailDto.builder()
+                .subject("Please activate your account")
+                .recipient(user.getEmail())
+                .body("Thank you for signing up to Implicaction, please click on the below url to activate your account : "
+                        + ACTIVATION_ENDPOINT + "/" + activationKey)
+                .build();
+        sendMail(notificationEmail);
     }
 }
