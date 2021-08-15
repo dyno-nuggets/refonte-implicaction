a4be3273ebdd9cd793dec26656cad681df868e98
matthieuaudemard <matthieu.audemard@gmail.com>
2021-08-15 20:25:02 +0200

    doc: ajoute les dépendances swagger et swagger-ui et implémente leur utilisation (#39)

--- backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplication.java
+++ backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplication.java
@@ -1,15 +1,18 @@
 package com.dynonuggets.refonteimplicaction;
 
+import com.dynonuggets.refonteimplicaction.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class RefonteImplicactionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RefonteImplicactionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RefonteImplicactionApplication.class, args);
+    }
 
 }
