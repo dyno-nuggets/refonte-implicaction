f2c1378afa19f8253747d0502754ffa21e8f8c7f
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-08-15 15:24:50 +0200

    test: ajoute les tests pour le UserRepository
     * ajout des dépendances aux TestContainers
     * test de la méthode save
     * test de la méthode findByUsername

---
+++ backend-implicaction/src/test/java/com/dynonuggets/refonteimplicaction/repository/UserRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.dynonuggets.refonteimplicaction.repository;
+
+import com.dynonuggets.refonteimplicaction.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest extends AbstractContainerBaseTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void shouldSaveUser() {
+        User user = User.builder()
+                .username("test user")
+                .password("secret password")
+                .email("user@email.com")
+                .registered(Instant.now())
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(user);
+    }
+
+    @Test
+    void shouldFindUser() {
+        User user = User.builder()
+                .username("test get user")
+                .password("secret password")
+                .email("userget@email.com")
+                .registered(Instant.now())
+                .build();
+
+        userRepository.save(user);
+        
+        Optional<User> byUsername = userRepository.findByUsername(user.getUsername());
+        assertThat(byUsername).isPresent();
+        assertThat(byUsername.get()).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(user);
+    }
+}
