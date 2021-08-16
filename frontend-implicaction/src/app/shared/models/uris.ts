99719515111c06d2a3c90b03f19d4097722f8a50
matthieuaudemard <matthieu.audemard@gmail.com>
2021-08-16 20:47:31 +0200

    feat: centralise les URI vers l'api dans la classe Uris (#42)

---
+++ frontend-implicaction/src/app/shared/models/uris.ts
@@ -0,0 +1,11 @@
+export class Uris {
+  /**
+   * Auth
+   */
+  static readonly AUTH = class {
+    static readonly LOGIN = 'auth/login';
+    static readonly SIGNUP = 'auth/signup';
+    static readonly REFRESH_TOKEN = 'auth/refresh/token';
+    static readonly LOGOUT = 'auth/logout';
+  };
+}
