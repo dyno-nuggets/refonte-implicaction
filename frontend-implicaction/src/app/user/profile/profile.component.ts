ca75255f64453a74298bd64d19c8b18d4bf1574f
Mélanie Da Costa <1161655-M3L_DC@users.noreply.gitlab.com>
2021-09-09 23:40:40 +0200

    feat(user/profile): ajoute WorkExperienceComponent
     * recupère le user id via les paramètre de la route
     * ajoute un lien sur le nom d'un user

--- frontend-implicaction/src/app/user/profile/profile.component.ts
+++ frontend-implicaction/src/app/user/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../shared/services/user.service";
 import {User} from "../../shared/models/user";
 import {ToasterService} from "../../core/services/toaster.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
@@ -9,18 +10,21 @@ import {ToasterService} from "../../core/services/toaster.service";
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  user: User;
+  user: User = {};
 
-  constructor(private userService: UserService, private toasterService: ToasterService) { }
+  constructor(private userService: UserService, private toasterService: ToasterService, private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
-    // TODO: Recuperer l'utilisateur en bd
-    this.userService
-      .getUser("13")
-      .subscribe(
-        user => this.user = user,
-        () => this.toasterService.error('oops', 'Une erreur est survenue !')
-      );
+    this.route.paramMap.subscribe(paramMap => {
+      const userId = paramMap.get('userId');
+      this.userService
+        .getUser(userId)
+        .subscribe(
+          user => this.user = user,
+          () => this.toasterService.error('oops', 'Une erreur est survenue !')
+        );
+    })
   }
 
 }
