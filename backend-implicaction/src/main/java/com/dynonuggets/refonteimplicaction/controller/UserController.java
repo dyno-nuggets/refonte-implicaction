c02c8e9bb70c970f1ed4e93e234991681e24abfd
PaFlu-SDV <86686893+PaFlu-SDV@users.noreply.github.com>
2021-09-21 09:35:02 +0200

    RI-31: sauvergarde les modifications apportées aux expériences professionelles (#87)
    * déplacement de la méthode de sauvegarde des modifications des trainings vers le user controller et suppression de trainingController

--- backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/controller/UserController.java
+++ backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/controller/UserController.java
@@ -1,19 +1,18 @@
 package com.dynonuggets.refonteimplicaction.controller;
 
+import com.dynonuggets.refonteimplicaction.dto.TrainingDto;
 import com.dynonuggets.refonteimplicaction.dto.UserDto;
-import com.dynonuggets.refonteimplicaction.service.AuthService;
-import com.dynonuggets.refonteimplicaction.service.RelationService;
-import com.dynonuggets.refonteimplicaction.service.UserService;
+import com.dynonuggets.refonteimplicaction.dto.WorkExperienceDto;
+import com.dynonuggets.refonteimplicaction.service.*;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -23,6 +22,8 @@ public class UserController {
     private final UserService userService;
     private final RelationService relationService;
     private final AuthService authService;
+    private final TrainingService trainingService;
+    private final WorkExperienceService workExperienceService;
 
     @GetMapping(params = {"page", "size"})
     public ResponseEntity<Page<UserDto>> getAll(
@@ -71,5 +72,17 @@ public class UserController {
         Long userId = authService.getCurrentUser().getId();
         Page<UserDto> usersDto = relationService.getReceivedFriendRequest(userId, pageable);
         return ResponseEntity.ok(usersDto);
+    }
+
+    @PutMapping(path = "/{userId}/trainings")
+    public ResponseEntity<List<TrainingDto>> updateAllTrainings(@RequestBody List<TrainingDto> trainingDtos, @PathVariable Long userId) {
+        List<TrainingDto> trainingUpdates = trainingService.updateByUserId(trainingDtos, userId);
+        return ResponseEntity.ok(trainingUpdates);
+    }
+
+    @PutMapping(path = "/{userId}/experiences")
+    public ResponseEntity<List<WorkExperienceDto>> updateALlExperiences(@RequestBody List<WorkExperienceDto> workExperienceDtos, @PathVariable Long userId) {
+        List<WorkExperienceDto> experienceUpdates = workExperienceService.updateByUserId(workExperienceDtos, userId);
+        return ResponseEntity.ok(experienceUpdates);
     }
 }
