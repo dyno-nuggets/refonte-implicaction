bc910eb8b7be2fe614ea57fbe3b36cb70fe38ce2
Mélanie Da Costa <1161655-M3L_DC@users.noreply.gitlab.com>
2021-09-22 18:18:21 +0200

    RI-26: Supprimer des expériences (dans le mode édition)

--- frontend-implicaction/src/app/user/components/experience-detail/experience-detail.component.ts
+++ frontend-implicaction/src/app/user/components/experience-detail/experience-detail.component.ts
@@ -1,21 +1,30 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {WorkExperience} from '../../../shared/models/work-experience';
+import {Utils} from '../../../shared/classes/utils';
 
 @Component({
-  selector: 'app-experience-detail',
-  templateUrl: './experience-detail.component.html',
-  styleUrls: ['./experience-detail.component.scss']
+    selector: 'app-experience-detail',
+    templateUrl: './experience-detail.component.html',
+    styleUrls: ['./experience-detail.component.scss']
 })
 export class ExperienceDetailComponent implements OnInit {
 
-  @Input()
-  experience: WorkExperience;
-  @Input()
-  isEditing: boolean;
+    @Input()
+    experience: WorkExperience;
+    @Input()
+    isEditing: boolean;
+    guid = Utils.generateGuid();
 
-  constructor() {
-  }
+    @Output()
+    onDelete = new EventEmitter<WorkExperience>();
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    deleteExperience(experience: WorkExperience): void {
+        this.onDelete.next(experience);
+    }
 }
