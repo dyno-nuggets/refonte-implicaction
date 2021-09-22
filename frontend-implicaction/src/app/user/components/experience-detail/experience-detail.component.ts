5d2bef702fab24176eb9a46fcf8b7d29c6e43bff
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-09-22 02:09:12 +0200

    RI-22: quelques optimisations

--- frontend-implicaction/src/app/user/components/experience-detail/experience-detail.component.ts
+++ frontend-implicaction/src/app/user/components/experience-detail/experience-detail.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {WorkExperience} from '../../../shared/models/work-experience';
-import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-experience-detail',
@@ -14,12 +13,9 @@ export class ExperienceDetailComponent implements OnInit {
   @Input()
   isEditing: boolean;
 
-  constructor(private userService: UserService) {
+  constructor() {
   }
 
   ngOnInit(): void {
   }
-
-  // TODO delete experience
-  // TODO add experience
 }
--- frontend-implicaction/src/app/user/components/experience-list/experience-list.component.ts
+++ frontend-implicaction/src/app/user/components/experience-list/experience-list.component.ts
@@ -1,38 +1,38 @@
-import {AfterContentChecked, Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {WorkExperience} from '../../../shared/models/work-experience';
 import {UserService} from '../../services/user.service';
 import {AuthService} from '../../../shared/services/auth.service';
 import {ToasterService} from '../../../core/services/toaster.service';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-experience-list',
   templateUrl: './experience-list.component.html',
   styleUrls: ['./experience-list.component.scss']
 })
-export class ExperienceListComponent implements OnInit, AfterContentChecked {
-  isEditing: boolean;
+export class ExperienceListComponent implements OnInit {
 
   @Input()
   experiences: WorkExperience[];
   experienceCopies: WorkExperience[];
   currentUserId: string;
+  isEditing = false;
 
   constructor(
     private userService: UserService,
     private authService: AuthService,
     private toasterService: ToasterService
   ) {
-    this.isEditing = false;
   }
 
-  ngAfterContentChecked(): void {
-  }
+  trackByExperienceId = (index: number, experience: WorkExperience) => experience.id;
 
   ngOnInit(): void {
     this.currentUserId = this.authService.getUserId();
   }
 
   toggleModeEdition(): void {
+    // on clone individuellement chaque élément de la liste pour ne pas impacter la liste experiences et pouvoir rollback
     this.experienceCopies = this.experiences.map(x => Object.assign({}, x));
     this.isEditing = !this.isEditing;
   }
@@ -40,24 +40,11 @@ export class ExperienceListComponent implements OnInit, AfterContentChecked {
   updateExperiences(): void {
     this.userService
       .updateExperiences(this.currentUserId, this.experienceCopies)
+      .pipe(finalize(() => this.isEditing = false))
       .subscribe(
-        experienceUpdates => {
-          console.log(experienceUpdates);
-          this.experiences = {...experienceUpdates};
-          this.experienceCopies = {...experienceUpdates};
-        },
-        error => {
-          this.toasterService.error('Oops', 'Une erreur est survenu lors de la mise à jour des données');
-          console.log(error);
-        },
-        () => this.toasterService.success('Succes', 'Le changement des données a bien été effectué')
+        experienceUpdates => this.experiences = [...experienceUpdates],
+        () => this.toasterService.error('Oops', 'Une erreur est survenu lors de la mise à jour des données'),
+        () => this.toasterService.success('Ok', 'Le changement des données a bien été effectué')
       );
-    this.toggleModeEdition();
   }
-
-  rollbackExperiences(): void {
-    this.experienceCopies = {...this.experiences};
-    this.toggleModeEdition();
-  }
-
 }
