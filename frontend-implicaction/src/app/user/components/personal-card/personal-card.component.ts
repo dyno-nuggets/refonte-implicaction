5d721ac492df19f38002430949382797c00c8493
dylanbrudey <47382098+dylanbrudey@users.noreply.github.com>
2021-09-23 23:32:41 +0200

    RI-20: ajoute un mode édition aux infos personnelles

--- frontend-implicaction/src/app/user/components/personal-card/personal-card.component.ts
+++ frontend-implicaction/src/app/user/components/personal-card/personal-card.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../../../shared/models/user';
+import {UserService} from '../../services/user.service';
+import {AuthService} from '../../../shared/services/auth.service';
+import {ToasterService} from '../../../core/services/toaster.service';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-personal-card',
@@ -10,11 +14,39 @@ export class PersonalCardComponent implements OnInit {
 
   @Input()
   user: User;
+  userCopies: User;
+  currentUserId: string;
+  isEditing = false;
 
-  constructor() {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private toasterService: ToasterService
+  ) {
   }
 
   ngOnInit(): void {
+    this.currentUserId = this.authService.getUserId();
   }
 
+  toggleModeEdition(): void {
+    if (!this.isEditing) {
+      // on clone le user afin de pouvoir rollback
+      this.userCopies = {...this.user};
+    } else {
+      this.user = {...this.userCopies};
+    }
+    this.isEditing = !this.isEditing;
+  }
+
+  updatePersonalInfo(): void {
+    this.userService
+      .updatePersonalInfo(this.currentUserId, this.userCopies)
+      .pipe(finalize(() => this.isEditing = false))
+      .subscribe(
+        personalInfoUpdates => this.user = {...personalInfoUpdates},
+        () => this.toasterService.error('Oops', 'Une erreur est survenue lors de la mise à jour des données'),
+        () => this.toasterService.success('Ok', 'Le changement des données a bien été effectué')
+      );
+  }
 }
