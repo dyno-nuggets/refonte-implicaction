e4b286f5239003f56c505208ef38f0ecc8edc743
dylanbrudey <47382098+dylanbrudey@users.noreply.github.com>
2021-10-03 00:31:29 +0200

    RI-101: Il est possible de modifier le profil d'un autre utilisateur
         *retour pr

--- frontend-implicaction/src/app/user/components/personal-card/personal-card.component.ts
+++ frontend-implicaction/src/app/user/components/personal-card/personal-card.component.ts
@@ -13,7 +13,7 @@ import {finalize} from 'rxjs/operators';
 export class PersonalCardComponent implements OnInit {
   readonly YEAR_RANGE = `1900:${new Date().getFullYear() + 1}`;
   @Input()
-  readOnly: boolean;
+  readOnly = true;
   @Input()
   user: User;
   userCopie: User;
