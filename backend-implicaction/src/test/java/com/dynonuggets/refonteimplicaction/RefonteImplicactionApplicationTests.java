96b02832b8c3afd1bb92275667d58ea04ac765a3
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-10-03 01:47:43 +0200

    fix: corrige les tests en erreurs
     * fourni un profil aux tests de l'application

--- backend-implicaction/src/test/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplicationTests.java
+++ backend-implicaction/src/test/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplicationTests.java
@@ -2,12 +2,14 @@ package com.dynonuggets.refonteimplicaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "local")
 class RefonteImplicactionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
