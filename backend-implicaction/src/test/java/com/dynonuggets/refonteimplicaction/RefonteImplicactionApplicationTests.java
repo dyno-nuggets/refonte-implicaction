401d176e4c0e5c8cde8dd167c303c25416085427
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-10-05 01:38:52 +0200

    fix: corrige les tests du contextLoad

--- backend-implicaction/src/test/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplicationTests.java
+++ backend-implicaction/src/test/java/com/dynonuggets/refonteimplicaction/RefonteImplicactionApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles(profiles = "local")
+@ActiveProfiles(profiles = "test")
 class RefonteImplicactionApplicationTests {
 
     @Test
