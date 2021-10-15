2c6c94ce24b0168860380343af4335e14dfeffb9
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-10-16 00:37:08 +0200

    RI-109: listing des offres
     * correction correction pr

--- frontend-implicaction/src/app/admin/jobs/admin-jobs.component.ts
+++ frontend-implicaction/src/app/admin/jobs/admin-jobs.component.ts
@@ -1,16 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-job',
   templateUrl: './admin-jobs.component.html',
   styleUrls: ['./admin-jobs.component.scss']
 })
-export class AdminJobsComponent implements OnInit {
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
+export class AdminJobsComponent {
 
 }
