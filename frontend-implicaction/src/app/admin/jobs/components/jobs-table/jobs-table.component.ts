8296d6b28db6f92e8406b761a945590fdac2dacb
nekorpeche <mt.mathusha@outlook.fr>
2021-11-04 20:56:36 +0100

    RI-131: edition d'une offre * ajout import du dropdown module

--- frontend-implicaction/src/app/admin/jobs/components/jobs-table/jobs-table.component.ts
+++ frontend-implicaction/src/app/admin/jobs/components/jobs-table/jobs-table.component.ts
@@ -10,6 +10,9 @@ import {JobCriteriaFilter} from '../../../../job/models/job-criteria-filter';
 import {JobPosting} from '../../../../shared/models/job-posting';
 import {JobPostingFormComponent} from '../job-posting-form/job-posting-form.component';
 import {SidebarService} from '../../../../shared/services/sidebar.service';
+import {SortDirectionEnum} from '../../../../shared/enums/sort-direction.enum';
+import {JobFilterContextService} from '../../../../job/services/job-filter-context.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-jobs-table',
@@ -25,13 +28,98 @@ export class JobsTableComponent {
   pageable: Pageable = Constants.PAGEABLE_DEFAULT;
   orderByEnums = JobSortEnum.all();
   selectedOrder = JobSortEnum.DATE_DESC;
-  jobCriteria: JobCriteriaFilter;
+  criteria: JobCriteriaFilter;
+  selectedOrderCode: string;
+  sortDirection = SortDirectionEnum;
 
   constructor(
     private jobService: JobService,
     private toastService: ToasterService,
-    private sidebarService: SidebarService
+    private sidebarService: SidebarService,
+    private filterService: JobFilterContextService,
+    private route: ActivatedRoute
   ) {
+  }
+
+  ngOnInit(): void {
+    this.pageable.sortOrder = JobSortEnum.DATE_DESC.sortDirection;
+    this.pageable.sortBy = JobSortEnum.DATE_DESC.sortBy;
+    this.selectedOrderCode = JobSortEnum.DATE_DESC.code;
+
+    this.filterService
+      .observeFilter()
+      .subscribe(criteria => {
+        this.criteria = criteria;
+        const objectParam = this.buildQueryParams();
+        this.filterService.updateRouteQueryParams(objectParam);
+        this.paginate();
+      });
+
+    this.getFilterFromQueryParams()
+      .then(() => this.filterService.setFilter(this.criteria));
+  }
+
+  paginate({page, first, rows} = this.pageable): void {
+    this.loading = true;
+    this.pageable.page = page;
+    this.pageable.first = first;
+    this.pageable.rows = rows;
+    this.jobService
+      .getAllByCriteria(this.pageable, this.criteria)
+      .pipe(finalize(() => this.loading = false))
+      .subscribe(
+        data => {
+          this.pageable.totalPages = data.totalPages;
+          this.pageable.totalElements = data.totalElements;
+          this.pageable.content = data.content;
+        },
+        () => this.toastService.error('Oops', 'Une erreur est survenue lors de la récupération de la liste des offres')
+      );
+  }
+
+  onSortChange({value}): void {
+    const selectedOrderField = JobSortEnum.from(value);
+    this.pageable.sortBy = selectedOrderField.sortBy;
+    this.pageable.sortOrder = selectedOrderField.sortDirection;
+    this.filterService.setFilter(this.criteria); // on relance la recherche en updatant le filtre
+  }
+
+  onSearchChange(): void {
+    this.filterService.setFilter(this.criteria);
+  }
+
+  private async getFilterFromQueryParams(): Promise<void> {
+    // TODO: voir si y'a un moyen plus élégant avec typeof
+    const filterKeys = ['search', 'contractType'];
+    const pageableKeys = ['rows', 'page', 'sortOrder', 'sortBy'];
+    return new Promise(resolve => {
+      this.route
+        .queryParams
+        .subscribe(params => {
+          Object.entries(params)
+            .forEach(([key, value]) => {
+              if (filterKeys.includes(key)) {
+                this.criteria[key] = value;
+              } else if (pageableKeys.includes(key)) {
+                this.pageable[key] = value;
+              }
+            });
+          return resolve();
+        });
+    });
+  }
+
+  /**
+   * @return any les filtres de recherche auxquels sont ajoutés les filtres de pagination
+   */
+  private buildQueryParams(): any {
+    return {
+      ...this.criteria,
+      size: this.pageable.rows,
+      page: this.pageable.page,
+      sortBy: this.pageable.sortBy,
+      sortOrder: this.pageable.sortOrder
+    };
   }
 
   loadJobs(event: LazyLoadEvent): void {
@@ -39,7 +127,7 @@ export class JobsTableComponent {
     const page = event.first / event.rows;
 
     this.jobService
-      .getAllByCriteria({page, rows: event.rows}, this.jobCriteria)
+      .getAllByCriteria({page, rows: event.rows}, this.criteria)
       .pipe(
         take(1),
         finalize(() => this.loading = false)
