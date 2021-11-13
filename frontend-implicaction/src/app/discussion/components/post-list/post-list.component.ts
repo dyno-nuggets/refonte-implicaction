d23f77b9b3d9c0343788c21d35c778c075f029de
matthieuaudemard <matthieu.audemard@gmail.com>
2021-11-13 16:31:06 +0100

    RI-165: [discussion] réorganise le module discussion (#172)
    * il intègre désormais la sidebar menu dans chacun de ses composants principaux

--- frontend-implicaction/src/app/discussion/components/post-list/post-list.component.ts
+++ frontend-implicaction/src/app/discussion/components/post-list/post-list.component.ts
@@ -1,14 +1,40 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Post} from '../../model/post';
+import {BaseWithPaginationComponent} from '../../../shared/components/base-with-pagination-component/base-with-pagination.component';
+import {PostService} from '../../services/post.service';
+import {ToasterService} from '../../../core/services/toaster.service';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent {
+export class PostListComponent extends BaseWithPaginationComponent<Post> implements OnInit {
 
-  @Input()
-  posts: Post[] = [];
+  constructor(
+    private postService: PostService,
+    private toastService: ToasterService
+  ) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.pageable.sortOrder = 'DESC';
+    this.paginate();
+  }
+
+  protected innerPaginate(): void {
+    this.postService
+      .getAllPosts(this.pageable)
+      .pipe(finalize(() => this.isLoading = false))
+      .subscribe(data => {
+          this.pageable.totalPages = data.totalPages;
+          this.pageable.totalElements = data.totalElements;
+          this.pageable.content = data.content;
+        },
+        () => this.toastService.error('Oops', 'Une erreur est survenue lors de la récupération de la liste des discussions')
+      );
+  }
 
 }
