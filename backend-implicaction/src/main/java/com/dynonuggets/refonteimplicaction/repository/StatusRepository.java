634a3a2ce456661b9a8799519331695f65cfdc66
nekorpeche <mt.mathusha@outlook.fr>
2021-11-14 19:55:58 +0100

    RI-136! Archivage d'un job + tests

--- backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/repository/StatusRepository.java
+++ backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/repository/StatusRepository.java
@@ -2,15 +2,6 @@ package com.dynonuggets.refonteimplicaction.repository;
 
 import com.dynonuggets.refonteimplicaction.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface StatusRepository extends JpaRepository<Status, Long> {
-
-    @Query("select s from Status s " +
-            "where (s.id= 2 and s.type = 'job_posting')")
-    Status getArchivedStatus();
-
-    @Query("select s from Status s " +
-            "where (s.id= 1 and s.type = 'job_posting')")
-    Status getUnarchivedStatus();
 }
