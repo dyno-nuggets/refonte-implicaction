8ca7f556f2de2a18a0391b513c0e7f49a42a19be
matthieuaudemard <matthieu.audemard@gmail.com>
2021-11-19 20:05:53 +0100

    RI-187: [jobs/detail] empêche les utilisateurs non premium à accéder au détail d'un job (#187)

--- backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/model/RoleEnum.java
+++ backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/model/RoleEnum.java
@@ -9,7 +9,8 @@ public enum RoleEnum {
     USER(1L, "USER", "ROLE_USER"),
     ADMIN(2L, "ADMIN", "ROLE_ADMIN"),
     JOB_SEEKER(3L, "JOB_SEEKER", "ROLE_JOB_SEEKER"),
-    RECRUITER(4L, "RECRUITER", "ROLE_RECRUITER");
+    RECRUITER(4L, "RECRUITER", "ROLE_RECRUITER"),
+    PREMIUM(5L, "PREMIUM", "ROLE_PREMIUM");
 
     private Long id;
     private String name;
--- frontend-implicaction/src/app/job/job-routing.module.ts
+++ frontend-implicaction/src/app/job/job-routing.module.ts
@@ -2,11 +2,17 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {JobDetailsComponent} from './components/job-details/job-details.component';
 import {JobsListComponent} from './components/jobs-list/jobs-list.component';
+import {AuthGuard} from '../core/guards/auth.guard.service';
+import {RoleEnumCode} from '../shared/enums/role.enum';
 
 const routes: Routes = [
   {
     path: ':jobId',
     component: JobDetailsComponent,
+    canActivate: [AuthGuard],
+    data: {
+      allowedRoles: [RoleEnumCode.PREMIUM, RoleEnumCode.ADMIN]
+    }
   },
   {
     path: '',
