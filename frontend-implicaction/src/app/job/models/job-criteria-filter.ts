95e8ecceb873baa781347dea4123efc1c8febf80
nekorpeche <mt.mathusha@outlook.fr>
2021-11-21 17:43:36 +0100

    RI-112: [admin/company] ajoute un composant de filtre au tableau des compagnies * code review

--- frontend-implicaction/src/app/job/models/job-criteria-filter.ts
+++ frontend-implicaction/src/app/job/models/job-criteria-filter.ts
@@ -1,5 +1,4 @@
 import {ContractEnumCode} from '../../shared/enums/contract.enum';
-import {ContractTypeCode} from './contract-type-enum';
 import {Criteria} from '../../shared/models/Criteria';
 
 export interface JobCriteriaFilter extends Criteria {
