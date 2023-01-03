2dd92251b88559bae476f7b4ac0a301d6e4fe7c9
amine <amine.djennadi@hotmail.fr>
2023-01-03 22:01:02 +0100

    [SGSA-99] style forum

--- frontend-implicaction/src/app/forums/forums.module.ts
+++ frontend-implicaction/src/app/forums/forums.module.ts
@@ -1,37 +1,35 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {PostListComponent} from './components/post-list/post-list.component';
-import {ForumsComponent} from './forums.component';
-import {ForumsRoutingModule} from './forums-routing.module';
-import {SharedModule} from '../shared/shared.module';
-import {PaginatorModule} from 'primeng/paginator';
-import {VoteboxComponent} from './components/votebox/votebox.component';
-import {AvatarModule} from 'primeng/avatar';
-import {PostTileComponent} from './components/post-tile/post-tile.component';
-import {PostDetailComponent} from './components/post-detail/post-detail.component';
-import {PostSkeletonComponent} from './components/post-skeleton/post-skeleton.component';
-import {SkeletonModule} from 'primeng/skeleton';
-import {OptionMenuComponent} from './components/option-menu/option-menu.component';
-import {CreateGroupFormComponent} from './components/create-group-form/create-group-form.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {TopGroupListingComponent} from './components/top-group-listing/top-group-listing.component';
-import {TopGroupSkeletonComponent} from './components/top-group-skeleton/top-group-skeleton.component';
-import {CommentListComponent} from './components/comment-list/comment-list.component';
-import {EditorModule} from 'primeng/editor';
-import {CommentTileComponent} from './components/comment-tile/comment-tile.component';
-import {CommentSkeletonComponent} from './components/comment-skeleton/comment-skeleton.component';
-import {CreatePostFormComponent} from './components/create-post-form/create-post-form.component';
-import {IconsModule} from '../icons/icons.module';
-import {GroupListComponent} from './components/group-list/group-list.component';
-import {TableModule} from 'primeng/table';
-import {GroupCardComponent} from './components/group-card/group-card.component';
-import {
-  CustomTableWithSearchBarComponent
-} from './components/custom-table-with-search-bar/custom-table-with-search-bar.component';
-import {ForumCardComponent} from './components/forum-card/forum-card.component';
-import {PostCardComponent} from './components/post-card/post-card.component';
-import {ButtonModule} from "primeng/button";
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { ForumsComponent } from './forums.component';
+import { ForumsRoutingModule } from './forums-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { PaginatorModule } from 'primeng/paginator';
+import { VoteboxComponent } from './components/votebox/votebox.component';
+import { AvatarModule } from 'primeng/avatar';
+import { PostTileComponent } from './components/post-tile/post-tile.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { PostSkeletonComponent } from './components/post-skeleton/post-skeleton.component';
+import { SkeletonModule } from 'primeng/skeleton';
+import { OptionMenuComponent } from './components/option-menu/option-menu.component';
+import { CreateGroupFormComponent } from './components/create-group-form/create-group-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TopGroupListingComponent } from './components/top-group-listing/top-group-listing.component';
+import { TopGroupSkeletonComponent } from './components/top-group-skeleton/top-group-skeleton.component';
+import { CommentListComponent } from './components/comment-list/comment-list.component';
+import { EditorModule } from 'primeng/editor';
+import { CommentTileComponent } from './components/comment-tile/comment-tile.component';
+import { CommentSkeletonComponent } from './components/comment-skeleton/comment-skeleton.component';
+import { CreatePostFormComponent } from './components/create-post-form/create-post-form.component';
+import { IconsModule } from '../icons/icons.module';
+import { GroupListComponent } from './components/group-list/group-list.component';
+import { TableModule } from 'primeng/table';
+import { GroupCardComponent } from './components/group-card/group-card.component';
+import { CustomTableWithSearchBarComponent } from './components/custom-table-with-search-bar/custom-table-with-search-bar.component';
+import { ForumCardComponent } from './components/forum-card/forum-card.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -66,8 +64,8 @@ import {ButtonModule} from "primeng/button";
     EditorModule,
     IconsModule,
     TableModule,
-    ButtonModule
-  ]
+    ButtonModule,
+    InputTextModule,
+  ],
 })
-export class ForumsModule {
-}
+export class ForumsModule {}
