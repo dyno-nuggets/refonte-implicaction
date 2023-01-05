7214bdacd01a761b13eeaa8eb58215fc566dbebe
Amine DJENNADI <amine.djennadi@ocapiat.fr>
2023-01-05 15:57:31 +0100

    [SGSA-92] début modif posts

--- frontend-implicaction/src/app/forums/components/post-tile/post-tile.component.ts
+++ frontend-implicaction/src/app/forums/components/post-tile/post-tile.component.ts
@@ -1,15 +1,14 @@
-import {Component, Input} from '@angular/core';
-import {Post} from '../../model/post';
-import {Univers} from '../../../shared/enums/univers';
-import {Constants} from '../../../config/constants';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../../model/post';
+import { Univers } from '../../../shared/enums/univers';
+import { Constants } from '../../../config/constants';
 
 @Component({
   selector: 'app-post-tile',
   templateUrl: './post-tile.component.html',
-  styleUrls: ['./post-tile.component.scss']
+  styleUrls: ['./post-tile.component.scss'],
 })
-export class PostTileComponent {
-
+export class PostTileComponent implements OnInit {
   @Input()
   post: Post = {};
   univers = Univers;
@@ -18,5 +17,7 @@ export class PostTileComponent {
   updateCommentCount(count: number): void {
     this.post.commentCount = count;
   }
-
+  ngOnInit(): void {
+    console.log(this.post);
+  }
 }
