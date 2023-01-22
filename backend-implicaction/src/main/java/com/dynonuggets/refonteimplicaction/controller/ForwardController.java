122f05e97ad70436f46d8184c60f26c95ecf7b5f
matthieu <matthieu.audemard@gmail.com>
2023-01-22 12:56:16 +0100

    refact:  modifie le ForwardController pour qu'il redéfinisse le BasicErrorController de spring boot

--- backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/controller/ForwardController.java
+++ backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/controller/ForwardController.java
@@ -1,14 +1,34 @@
 package com.dynonuggets.refonteimplicaction.controller;
 
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
 
 @Controller
-public class ForwardController {
+public class ForwardController extends BasicErrorController {
 
-    @GetMapping(value = "/**/{[path:[^^.]*}")
-    public String redirect() {
-        // redirige vers la home page pour préserver la route en cas de rafraichissement de la page
-        return "forward:/";
+    public ForwardController(ErrorAttributes errorAttributes) {
+        super(errorAttributes, new ErrorProperties());
+    }
+
+    @Override
+    @RequestMapping(produces = TEXT_HTML_VALUE)
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        HttpStatus status = getStatus(request);
+        if (NOT_FOUND.equals(status)) {
+            return new ModelAndView("forward:/");
+        } else {
+            return super.errorHtml(request, response);
+        }
     }
 }
