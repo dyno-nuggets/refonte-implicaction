5cb5a65de08270c4bac62802a4321a13f33eb75c
gillout <54893913+gillout@users.noreply.github.com>
2023-04-09 22:35:18 +0200

    RI-244: permet de garder le panel à l'écran lorsque la souris le survole

--- frontend-implicaction/src/app/shared/components/profile-info-display/profile-info-display.component.ts
+++ frontend-implicaction/src/app/shared/components/profile-info-display/profile-info-display.component.ts
@@ -1,16 +1,33 @@
-import {Component, Input} from '@angular/core';
-import {Profile} from "../../../profile/models/profile";
+import {Component, Input, OnDestroy, ViewChild} from '@angular/core';
 import {Univers} from "../../enums/univers";
+import {Profile} from "../../../community/models/profile/profile";
+import {Subscription, timer} from "rxjs";
+import {OverlayPanel} from "primeng/overlaypanel";
 
 @Component({
   selector: 'app-profile-info-display',
   templateUrl: './profile-info-display.component.html',
   styleUrls: ['./profile-info-display.component.scss']
 })
-export class ProfileInfoDisplayComponent {
-  @Input()
-  profile?: Profile;
+export class ProfileInfoDisplayComponent implements OnDestroy {
 
-  univer = Univers;
+  private static readonly HIDE_TIMER_DELAY = 200;
+  @Input() profile?: Profile;
+  @ViewChild('panel') private panel: OverlayPanel;
 
+  univers = Univers;
+
+  private subscription: Subscription;
+
+  initPanelHide() {
+    this.subscription = timer(ProfileInfoDisplayComponent.HIDE_TIMER_DELAY).subscribe(() => this.panel.hide());
+  }
+
+  cancelPanelHideOperation() {
+    this.subscription?.unsubscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
