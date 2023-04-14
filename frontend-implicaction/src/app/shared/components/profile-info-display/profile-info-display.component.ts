fba34f6e3b3931037b16d0a9cf485ff3f83cd8e9
gtaigniere <54893913+gtaigniere@users.noreply.github.com>
2023-04-14 21:10:43 +0200

    RI-244: ajoute un overlay-panel qui présente les infos de l'utilisateur (#295)
    Co-authored-by: gillout <54893913+gillout@users.noreply.github.com>
    Co-authored-by: matthieuaudemard <matthieu.audemard@gmail.com>

---
+++ frontend-implicaction/src/app/shared/components/profile-info-display/profile-info-display.component.ts
@@ -0,0 +1,49 @@
+import {ChangeDetectionStrategy, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Univers} from "../../enums/univers";
+import {Profile} from "../../../community/models/profile/profile";
+import {Subscription, timer} from "rxjs";
+import {OverlayPanel} from "primeng/overlaypanel";
+
+@Component({
+  selector: 'app-profile-info-display',
+  templateUrl: './profile-info-display.component.html',
+  styleUrls: ['profile-info-display.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProfileInfoDisplayComponent implements OnInit {
+
+  private static readonly HIDE_TIMER_DELAY = 200;
+  private static readonly SHOW_TIMER_DELAY = 600;
+
+  @Input() profile?: Profile;
+
+  @ViewChild('panel') private panel: OverlayPanel;
+
+  profileLink = [];
+  transitionOptions = '.0s';
+
+  private subscriptionHide: Subscription;
+  private subscriptionShow: Subscription;
+
+  ngOnInit(): void {
+    this.profileLink = ['/', Univers.COMMUNITY.url, 'profile', this.profile.username];
+  }
+
+  initPanelHideOperation() {
+    this.subscriptionShow?.unsubscribe();
+    this.subscriptionHide = timer(ProfileInfoDisplayComponent.HIDE_TIMER_DELAY).subscribe(
+      () => this.panel.hide()
+    );
+  }
+
+  initPanelShowOperation(event) {
+    this.subscriptionHide?.unsubscribe();
+    this.subscriptionShow = timer(ProfileInfoDisplayComponent.SHOW_TIMER_DELAY).subscribe(
+      () => this.panel.show(event)
+    );
+  }
+
+  cancelPanelHideOperation() {
+    this.subscriptionHide?.unsubscribe();
+  }
+}
