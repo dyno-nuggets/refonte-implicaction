16649623477761fc88af4066cb5df06409f45bd0
matthieuaudemard <matthieu.audemard@gmail.com>
2023-05-08 02:04:42 +0200

    refact: modifie le sidemenu pour qu'il puisse inclure des collapsibles

--- frontend-implicaction/src/app/shared/components/sidemenu/sidemenu.component.ts
+++ frontend-implicaction/src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -10,13 +10,23 @@ export class SidemenuComponent {
 
   @Input() menuItems: MenuItem[];
 
-  toggleCollapseParent(child: MenuItem, parent: MenuItem, expanded: boolean) {
-    child.expanded = expanded;
+  /**
+   * Pour développer le parent, on regarde s’il possède au moins un enfant 'active' cad si l’attribut
+   * isActive est vrai.
+   */
+  onChildActiveChange(child: MenuItem, parent: MenuItem, isActive: boolean) {
+    child.expanded = isActive;
     parent.expanded = parent.items.some(item => item.expanded);
   }
 
+
   toggleCollapse(menuItem: MenuItem) {
-    this.menuItems.filter(i => i !== menuItem).forEach(i => i.expanded = false);
+    this.menuItems
+      .filter(item => item !== menuItem && item.expanded)
+      .forEach(item => {
+        item.expanded = false;
+      });
+
     menuItem.expanded = !menuItem.expanded;
   }
 }
